73cbc652e6d9cc6400224ccc7f3c8a52dd183825
Arthur <amalangin@icloud.com>
2024-10-28 16:25:49 +0100

    Fix sigev, err readofsize when client leave server without leave chanel

--- src/Server/commands/quit.cpp
+++ src/Server/commands/quit.cpp
@@ -12,8 +12,28 @@
 
 #include "../../../include/Server.hpp"
 
+void elpueblo() {
+
+}
+
 void Server::commandQUIT(int fd)
 {
+	Client &client = getClientByFd(fd);
+
+	for (size_t d = 0; d < client.getChannelList().size(); d++)
+	{
+		Channel *channel = client.getChannelList()[d];
+
+		std::vector<Client *>users = channel->getClientList();
+
+		for (size_t i = 0; i < users.size(); i++) {
+			if (fd != users[i]->getFd()) {
+				sendMessage(users[i]->getFd(), ":" + client.getNick() + "!~" + client.getUser() + "@" +
+				client.getIp() + ".ip QUIT :Quit: Leaving\r\n");
+			}
+		}
+		client.getChannelList()[d]->rmClient(client);
+	}
 	std::cout << "\e[1;31m" << "Client <" << fd << "> Disconnected !" << "\e[0;37m" << std::endl;
 	disconnectClientByFd(fd);
 }
