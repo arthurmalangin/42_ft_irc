9fade0b4e7cab425e1794ee8d389088ccd72e709
arthurmalangin <amalangin@icloud.com>
2024-12-07 05:04:38 +0100

    fix auth when user can have the same nickname

--- src/Server/authentication.cpp
+++ src/Server/authentication.cpp
@@ -19,47 +19,50 @@ void Server::authentication(int fd, const char *buffer)
 	parser.parseBuffer(buffer);
 
 	Client &client = getClientByFd(fd);
-
-	if (!client.getAuth())
+	bool passTmp = false;
+	for (size_t i = 0; i < parser.message.size(); i++)
 	{
-		for (size_t i = 0; i < parser.message.size(); i++)
+		if ((parser.message[i][0] == "NICK" || parser.message[i][0] == "USER") && parser.message[i].size() > 1)
 		{
-			if ((parser.message[i][0] == "NICK" || parser.message[i][0] == "USER") && parser.message[i].size() > 1)
+			if (parser.message[i][1].find('#') != std::string::npos)
 			{
-				if (parser.message[i][1].find('#') != std::string::npos)
-				{
-					sendMessage(fd, ":server 401 " + parser.message[i][1] + " :Invalid name. The character '#' is not allowed.\r\n");
-					std::cout << "\e[1;31m" << "Client <" << fd << "> Disconnected for bad char in name !" << "\e[0;37m" << std::endl;
+				sendMessage(fd, ":server 401 " + parser.message[i][1] + " :Invalid name. The character '#' is not allowed.\r\n");
+				std::cout << "\e[1;31m" << "Client <" << fd << "> Disconnected for bad char in name !" << "\e[0;37m" << std::endl;
+				disconnectClientByFd(fd);
+				return ;
+			}
+		}
+		if (parser.message[i][0] == "NICK" && parser.message[i].size() > 1)
+			client.setNick(parser.message[i][1]);
+		if (parser.message[i][0] == "USER" && parser.message[i].size() > 1)
+			client.setUser(parser.message[i][1]);
+		if (parser.message[i][0] == "PASS" && parser.message[i].size() > 1 && parser.message[i][1] == this->_password)
+			passTmp = true;
+	}
+	if (!client.getNick().empty() && !client.getUser().empty() && passTmp) {
+		for (size_t j = 0; j < this->_clientList.size(); j++) {
+			Client &clientCheck = *this->_clientList[j];
+			if (clientCheck.getFd() != client.getFd()) { // car sinon l'user checkera avec lui meme et se fera kick en boucle
+				if (client.getNick() == clientCheck.getNick() || client.getUser() == clientCheck.getUser()) {
+					// std::cout << "client fd: " << client.getFd() << " clientCheckFd: " << clientCheck.getFd() << std::endl;
+					// std::cout << "=========info========" << std::endl;
+					// std::cout << "NICK = " << client.getNick() << " clientCheck NICK = " << clientCheck.getNick() << std::endl;
+					// std::cout << "USER = " << client.getUser() << " clientCheck USER = " << clientCheck.getUser() << std::endl;
+					sendMessage(fd, ":MyChell.beer 433 ERR_AUTH " + client.getNick() + ": Nickname or Username is already in use\r\n");
+					std::cout << "\e[1;31m" << "Client <" << fd << "> Disconnected for ERR_AUTH !" << "\e[0;37m" << std::endl;
 					disconnectClientByFd(fd);
 					return ;
 				}
-				for (size_t j = 0; j < this->_clientList.size(); j++)
-				{
-					if ((parser.message[i][1] == "NICK" ? this->_clientList[j]->getNick() : this->_clientList[j]->getUser()) == parser.message[i][1])
-					{
-						sendMessage(fd, ":MyChell.beer 433 ERR_NICKNAMEINUSE " + parser.message[i][1] + ": Nickname is already in use\r\n");
-						std::cout << "\e[1;31m" << "Client <" << fd << "> Disconnected for ERR_NICKNAMEINUSE !" << "\e[0;37m" << std::endl;
-						disconnectClientByFd(fd);
-						return ;
-					}
-				}
-			}
-			if (parser.message[i][0] == "NICK" && parser.message[i].size() > 1)
-				client.setNick(parser.message[i][1]);
-			if (parser.message[i][0] == "USER" && parser.message[i].size() > 1)
-				client.setUser(parser.message[i][1]);
-			if (parser.message[i][0] == "PASS" && parser.message[i].size() > 1 && parser.message[i][1] == this->_password)
-			{
-				client.setAuth(true);
-				commandMOTD(fd);
-				std::cout << "\e[1;32m" << "Client <" << fd << "> Auth Success !" << std::endl;
 			}
 		}
-		if (!client.getAuth())
-		{
-			sendMessage(fd, ":MyChell.beer 464 : Mot de passe Incorrect\r\n");
-			std::cout << "\e[1;31m" << "Client <" << fd << "> Disconnected for Auth Fail !" << "\e[0;37m" << std::endl;
-			disconnectClientByFd(fd);
-		}
+		client.setAuth(true);
+		commandMOTD(fd);
+		std::cout << "\e[1;32m" << "Client <" << fd << "> Auth Success !" << std::endl;
+	}
+	if (!client.getAuth())
+	{
+		sendMessage(fd, ":MyChell.beer 464 : Mot de passe Incorrect\r\n");
+		std::cout << "\e[1;31m" << "Client <" << fd << "> Disconnected for Auth Fail !" << "\e[0;37m" << std::endl;
+		disconnectClientByFd(fd);
 	}
 }
